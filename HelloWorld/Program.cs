1aa50958459037483fbb1cc07815bfa47ba2ecd7
Luis Mendez <luismendezescobar@gmail.com>
2024-03-06 21:16:34 -0600

    testing push-08

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,8 +12,7 @@ namespace HelloWorld
 
         public static string Greet()
         {
-            return "Hello, World!!!!!";        
-        
+            return "Hello, World!!!!!";                
         }
     }
 }
